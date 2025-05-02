f52cd326ddce82fbd7f026c888a2155efb380420
Cameron Chetty <ST10251759@vcconnect.edu.za>
2025-05-02 21:15:53 +0200

    Added Assets in www root, created a Home Page and About Us Page with static html and css styling.  Created a general Layout and Theme for my application.

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10251759_PROG7313_POE_PART_2.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
